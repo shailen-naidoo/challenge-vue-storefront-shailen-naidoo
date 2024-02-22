23a595b0753e799842afaed5153ebcddc1ce8a69
Shailen Naidoo <naidooshailen648@gmail.com>
2024-02-22 14:43:27 +0200

    package.json, package-lock.json
    * Installed the official React Router
    
    router.jsx
    
    * Setup a simple router for now
    
    main.jsx
    
    * Added the router as apart of the component tree

---
+++ router.jsx
@@ -0,0 +1,14 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import App from './src/App'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} />
+  )
+)
+
+export { router }
