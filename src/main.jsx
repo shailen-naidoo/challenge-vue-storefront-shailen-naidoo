8c313536ffbae57778e23a05c6a3abf8487702c3
Shailen Naidoo <naidooshailen648@gmail.com>
2024-02-22 15:15:17 +0200

    main.jsx
    * I copied the application shell from the Vue Storefront and made some changes but everything looks perfectly fine

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,14 @@ import { router } from '../router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <nav className="bg-gray-800">
+      <section className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <router-link to="/" class="font-bold text-white text-2xl">DVT Vue Storefront</router-link>
+        <ul className="hidden md:flex space-x-4 text-white">
+          <li></li>
+        </ul>
+      </section>
+    </nav>
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
