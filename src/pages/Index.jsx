34081d1c7a5671698f8c7222fe83f15b1fd49695
Shailen Naidoo <naidooshailen648@gmail.com>
2024-02-22 16:04:32 +0200

    src/pages/Index.jsx
    * Added the ability for the user to select the quantity of the product

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from 'react'
 import { ProductService } from '../services/ProductService'
+import { Fragment } from 'react'
 
 // eslint-disable-next-line react/prop-types
-function ProductCard({ image, title, category, description, price }) {
+function ProductCard(product, index, { updateProducts }) {
   return (
-    <>
+    <Fragment key={index}>
       <article className="card bg-white rounded-md p-4 shadow-md hover:shadow-lg cursor-pointer">
-        <img src={image} className="w-full h-48 object-cover" />
-        <h3 className="text-lg font-medium mt-4">{title}</h3>
-        <h4 className="text-lg font-medium mt-4">{category}</h4>
-        <p className="text-gray-500 text-sm mb-2">{description}</p>
-        <span className="font-bold text-green-500">R{price}</span>
+        <img src={product.image} className="w-full h-48 object-cover" />
+        <h3 className="text-lg font-medium mt-4">{product.title}</h3>
+        <h4 className="text-lg font-medium mt-4">{product.category}</h4>
+        <p className="text-gray-500 text-sm mb-2">{product.description}</p>
+        <span className="font-bold text-green-500">R{product.price}</span>
+        <select value={product.quantity} className="ml-4" name="Product Quantity" onChange={(e) => updateProducts(e, index)}>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
       </article>
-    </>
+    </Fragment>
   )
 }
 
@@ -23,11 +31,19 @@ function App() {
     ProductService.fetchListOfProducts().then(setProducts)
   }, [])
 
+  function updateProducts(e, index) {
+    const cloneOfProducts = [...products]
+
+    cloneOfProducts[index].quantity = parseInt(e.target.value)
+
+    setProducts(cloneOfProducts)
+  }
+
   return (
     <>
       <main className="container mx-auto">
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {products.map(ProductCard)}
+          {products.map((products, index) => ProductCard(products, index, { updateProducts }))}
         </section>
       </main>
     </>
